f34ab051d32534c504e2850078ca85059af85ce1
Nikita7720 <prajapatinikita050@gmail.com>
2026-07-15 20:44:38 +0530

    Delete Banking_App - Copy directory

--- Banking_App - Copy/bank/src/main/java/com/example/bank/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank.repository;
-
-import com.example.bank.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUserIdOrderByDateTimeDesc(Long userId);
-}
--- Banking_App - Copy/bank/src/main/java/com/example/bank/repository/StaffSalaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank.repository;
-
-import com.example.bank.entity.StaffSalary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StaffSalaryRepository extends JpaRepository<StaffSalary, Long> {
-    List<StaffSalary> findByStaffIdOrderByPaymentDateDesc(Long staffId);
-}
